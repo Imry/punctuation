94e2bdd7ee3e2ed66bf547c07d4ef387148672e7
Imry <chester@list.ru>
2016-10-31 03:22:17 +0300

    .

--- parse.py
+++ parse.py
@@ -1,96 +1,94 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
 import re
 
 import pymorphy2
 
-# from parsimonious.grammar import Grammar
-# from parsimonious.nodes import NodeVisitor
-# import parsimonious
+# from peg import PEGParser, Rule
+import peg
 
-from peg_grammar import RuleParser
+class TextParser:
+    class Template:
+        def __init__(self, text, handler):
+            self.text = text
+            self.handler = handler
 
-class Rule:
-    def __init__(self, rule, pattern, repl):
-        self.text = rule
-        self.pattern = pattern
-        self.repl = repl
+        def __call__(self, sentence):
+            if self.handler:
+                return self.handler(sentence)
+            else:
+                return sentence
 
-    def __call__(self, sentence):
-        return re.sub(self.pattern, self.repl, sentence)
+        def __str__(self):
+            return self.handler.str()
 
-    def __str__(self):
-        return '-> \'%s\'\n<-\'%s\''%(self.pattern, self.repl)
+    class Rule:
+        def __init__(self, name, tmpl = None, text = None):
+            self.name = name
+            if tmpl:
+                self.tmpl = tmpl
+            else:
+                self.tmpl = []
+            if text:
+                self.text = text
+            else:
+                self.text = []
 
-class textParser:
-    def __init__(self, parser):
+        def __call__(self, sentence):
+            r = []
+            for t in self.tmpl:
+                s = t(sentence)
+                if s != sentence:
+                    r.append((self.name, t.text, s))
+                sentence = s
+            return sentence, r
+
+        def from_json(self, json):
+            self.name = json['name']
+            self.text = json['text']
+
+        def to_json(self):
+            data = {}
+            data['name'] = self.name
+            data['text'] = self.text
+            return data
+
+    def __init__(self, parser = peg.PEGParser):
         self.rules = []
         self.parser = parser()
         self.morph = pymorphy2.MorphAnalyzer()
 
-    def prepare_sentence(self, sentence):
-        r = []
-        for w in sentence.strip().split():
-            m = self.morph.parse(w)[0].tag
-            r.append('%s(%s)'%(w, m))
-        return ' '.join(r)
+    # def add_rule(self, rule):
+    #     self.rules.append(rule)
+    #     return self.rules
 
-    def norm_sentence(self, sentence):
-        return re.sub(r'([^\(\)]*?)\([^\(\)]*\)', r'\1', sentence)
+    # def get_rule(self, name):
+    #     for r in self.rules:
+    #         if r.name == name:
+    #             return r
 
-    def parse(self, sentence):
-        # print(sentence)
-        sentence = self.prepare_sentence(sentence)
-        # print(sentence)
-        for r in self.rules:
-            ss = r(sentence)
+    def get_morph_data(self, sentence):
+        return [self.morph.parse(w)[0].tag for w in sentence.strip().split()]
 
-            # Велосипед
-            ss = re.sub(r',,', r',', ss)
-            ss = re.sub(r'^,', r'', ss)
-            ss = re.sub(r',$', r'', ss)
-            # ss = re.sub(r'  ', r' ', ss)
-
-            if ss != sentence:
-                print('%s: %s'%(r.text, self.norm_sentence(ss)))
-            else:
-                # print('.')
-                None
+    def parse(self, sentence, rules = None):
+        if not rules:
+            rules = self.rules
+        sentence = self.parser.prepare_sentence(sentence, self.get_morph_data(sentence))
+        res = []
+        for r in rules:
+            ss, chain = r(sentence)
+            if chain != []:
+                for c in chain:
+                    res.append((c[0], c[1], self.parser.norm_sentence(c[2])))
             sentence = ss
-        # print(sentence)
-        return self.norm_sentence(sentence)
+        return self.parser.norm_sentence(sentence), res
 
-    def add(self, rule_str):
-        print(rule_str)
-        r = self.parser.parse_rule(rule_str)
-        r = Rule(rule_str, r[0], r[1])
-        print(r)
-        self.rules.append(r)
+    def parse_rule(self, rule):
+        return self.parser.parse_rule(rule)
 
-if __name__ == '__main__':
-    rules = ['(+,)(вернее|точнее)(+,)',
-            '(+,) ( вернее|точнее) (-, ) (говоря|сказать)(+,)',
-            '(показания|узнаю)(-,)(точнее)(-,)',
-            '(-,)(точнее)(-,)(показания|узнаю)',
-            '(по)(-,)(точнее)(-,)']
-
-    # text = ['он познакомился с ними вернее говоря они познакомились с ним']
-    text = ['он познакомился с ними вернее они познакомились с ним',
-            'он познакомился с ними вернее сказать они познакомились с ним',
-            'его показания точнее всех я им больше верю',
-            'сейчас погоди узнаю точнее',
-            'точнее узнаю и тебе позвоню',
-            'узнай пожалуйста по точнее'
-            ]
-
-    tp = textParser(RuleParser)
-    for r in rules:
-        tp.add(r)
-
-    print()
-
-    for t in text:
-        print('-> \'%s\''%(t))
-        print('<- \'%s\''%(tp.parse(t)))
+    # def add(self, rule):
+    #     r = self.parser.parse_rule(rule)
+    #     if r:
+    #         self.rules.append(r)
+    #     return r
